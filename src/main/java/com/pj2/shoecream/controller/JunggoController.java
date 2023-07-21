7fb9fc05dbcc112ae8cd3343f293275bf0cb17a2
BaeKILB <backgroundp7@gmail.com>
2023-07-21 09:46:40 +0900

    배경인 - 중고 컨트롤러 추가

--- src/main/java/com/pj2/shoecream/controller/JunggoController.java
+++ src/main/java/com/pj2/shoecream/controller/JunggoController.java
@@ -14,5 +14,18 @@ public class JunggoController {
 	public String junggoSearch(@RequestParam Map<String,Object> map, Model model) {
 		return "";
 	}
+	@GetMapping("JungChat")
+	public String jungChat(@RequestParam Map<String,Object> map, Model model) {
+		return "";
+	}
+	@GetMapping("JunggoPay")
+	public String junggoPay(@RequestParam Map<String,Object> map, Model model) {
+		return "";
+	}
+	@GetMapping("JunggoResults")
+	public String junggoResults(@RequestParam Map<String,Object> map, Model model) {
+		return "";
+	}
+	
 	
 }
