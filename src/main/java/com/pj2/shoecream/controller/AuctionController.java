9a11b38e3ebbdf3a3a2036793cb4e97ea19fe7dd
onbada <135286210+onbada@users.noreply.github.com>
2023-07-24 13:56:42 +0900

    0724 이온

--- src/main/java/com/pj2/shoecream/controller/AuctionController.java
+++ src/main/java/com/pj2/shoecream/controller/AuctionController.java
@@ -3,6 +3,7 @@ package com.pj2.shoecream.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.pj2.shoecream.service.AuctionService;
 
@@ -15,4 +16,15 @@ public class AuctionController {
 	public String auctionMain() {
 		return "auction/auction_main";
 	}
+	
+    @PostMapping("AuctionDetail")
+    public String AuctionDetail() { //경매 제품상세
+        return "";
+    }
+
+    @GetMapping("AuctionRecord")
+    public String AuctionRecord() { //경매 기록
+        return "";
+    }
+    
 }
