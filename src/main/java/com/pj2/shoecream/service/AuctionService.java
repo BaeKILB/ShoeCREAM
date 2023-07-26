c3949bc1ea928728cf72f0b36f132b641eca4358
onbada <135286210+onbada@users.noreply.github.com>
2023-07-26 18:03:50 +0900

    0726 이온
    경매 상세페이지 작업중

--- src/main/java/com/pj2/shoecream/service/AuctionService.java
+++ src/main/java/com/pj2/shoecream/service/AuctionService.java
@@ -1,8 +1,11 @@
 package com.pj2.shoecream.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuctionService {
-	
+
+
 }
--- src/main/webapp/resources/js/auction/auction_detail.js
+++ src/main/webapp/resources/js/auction/auction_detail.js
@@ -13,3 +13,44 @@ function tenderPopup() {
 
 function buyingPopup() { 
 	window.open("buying_popup.jsp", "즉시구매하기", "width=400, height=800, left=100, top=50"); }
+	
+	
+//삭제버튼 클릭시	
+function confirmDelete() {
+let isDelete = confirm("게시물을 삭제하시겠습니까?");
+
+	// isDelete 가 true 일 때 BoardDelete 서블릿 요청
+	if(isDelete) {
+		location.href='BoardDelete?board_num=${board.board_num}&pageNum=${param.pageNum}';
+	}
+}
+
+//이미지 슬라이드
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
