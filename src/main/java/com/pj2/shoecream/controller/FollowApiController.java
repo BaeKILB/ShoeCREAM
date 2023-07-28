bbd4f81a6db2ddf8247ddc1bd0406e631efe7c4a
oranc1226 <oranc1226@naver.com>
2023-07-28 17:51:24 +0900

    0728 김태윤 - 소셜 팔로우 구성 중

---
+++ src/main/java/com/pj2/shoecream/controller/FollowApiController.java
@@ -0,0 +1,54 @@
+package com.pj2.shoecream.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pj2.shoecream.config.PrincipalDetails;
+import com.pj2.shoecream.service.FollowService;
+import com.pj2.shoecream.vo.CMRespDto;
+import com.pj2.shoecream.vo.FollowVO;
+
+@RestController
+public class FollowApiController {
+	
+	@Autowired
+	private FollowService followService;
+	
+	@PostMapping("/api/follow/{followee_idx}")
+	public ResponseEntity<?> follow(
+			FollowVO Follow, @AuthenticationPrincipal PrincipalDetails principalDetails,
+			@PathVariable int followee_idx, BindingResult bindingResult) {
+//		int insertCount = followService.registFollow(follower_idx, followee_idx);
+		// 1. 기존 방식
+//		int insertCount = followService.registFollow(principalDetails.getMember().getMem_idx(), followee_idx);
+//		return new ResponseEntity<>(new CMRespDto<>(insertCount, "구독하기 성공", null), HttpStatus.OK);
+		// 2. 다른 방식
+		// 세션 정보 가져오기
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();
+        
+		followService.registFollow(mPrincipalDetails.getMember().getMem_idx(), followee_idx);
+		return new ResponseEntity<>(new CMRespDto<>(1, "구독하기 성공", null), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/api/subscribe/{followee_idx}")
+	public ResponseEntity<?> unfollow(@AuthenticationPrincipal PrincipalDetails principalDetails,
+			@PathVariable int followee_idx) {
+		// 1. 기존 방식
+//		int deleteCount = followService.deleteFollow(principalDetails.getMember().getMem_idx(), followee_idx);
+//		return new ResponseEntity<>(new CMRespDto<>(deleteCount, "구독취소하기 성공", null), HttpStatus.OK);
+		// 2. 다른 방식
+		followService.deleteFollow(principalDetails.getMember().getMem_idx(), followee_idx);
+		return new ResponseEntity<>(new CMRespDto<>(1, "구독취소하기 성공", null), HttpStatus.OK);
+		
+	}
+}
