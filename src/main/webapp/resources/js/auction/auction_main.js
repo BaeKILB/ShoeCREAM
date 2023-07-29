8f0ea8c9db52be7146610ba81b355073bbef7be3
PC122918200\Administrator <dbstjdgur0@gmail.com>
2023-07-29 17:19:58 +0900

    0729 윤성혁 - 경매 메인작업중

--- src/main/webapp/resources/js/auction/auction_main.js
+++ src/main/webapp/resources/js/auction/auction_main.js
@@ -59,6 +59,7 @@ const doneResult = data => {
     console.log(pageInfo)
     
 	let index = 0;
+	console.log(data[0]);
     for (let item of data) {
         let result =
             "<a href='AuctionDetail?auction_idx="+ item.auction_idx +"'>"
@@ -97,23 +98,28 @@ const updateTimer = (idx, data) => {
 	const now = new Date();
 	const diff = future - now;
 	
-	const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-	const hours = Math.floor(diff / (1000 * 60 * 60));
-	const mins = Math.floor(diff / (1000 * 60));
-	const secs = Math.floor(diff / 1000);
+	if (diff < 0) {
+		$("#remainingTime"+idx).html("경매 마감");
+	} else {
+		const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+		const hours = Math.floor(diff / (1000 * 60 * 60));
+		const mins = Math.floor(diff / (1000 * 60));
+		const secs = Math.floor(diff / 1000);
+		
+		const d = days;
+		const h = hours - days * 24;
+		const m = mins - hours * 60;
+		const s = secs - mins * 60;
+		
+		let result =  
+		'<span>' + d + '<span>일</span>' +
+		'<span>' + h + '<span>시</span>' +
+		'<span>' + m + '<span>분</span>' +
+		'<span>' + s + '<span>초</span>';
+		
+		$("#remainingTime"+idx).html(result);
+	}
 	
-	const d = days;
-	const h = hours - days * 24;
-	const m = mins - hours * 60;
-	const s = secs - mins * 60;
-	
-	let result =  
-	'<span>' + d + '<span>일</span>' +
-	'<span>' + h + '<span>시</span>' +
-	'<span>' + m + '<span>분</span>' +
-	'<span>' + s + '<span>초</span>';
-	
-	$("#remainingTime"+idx).html(result)
 }
 
 setInterval(() => {
