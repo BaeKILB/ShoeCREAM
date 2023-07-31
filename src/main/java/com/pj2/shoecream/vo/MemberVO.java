20d813955680173c18df9301b2415b9289277a4f
oranc1226 <oranc1226@naver.com>
2023-07-31 17:11:11 +0900

    0731 김태윤 - 비밀번호 찾기

--- src/main/java/com/pj2/shoecream/vo/MemberVO.java
+++ src/main/java/com/pj2/shoecream/vo/MemberVO.java
@@ -18,7 +18,7 @@ public class MemberVO {
 	// 회원 항목 추가
 	private int mem_idx;
 	
-	@ValidNonConflictId // 아이디 중복 검사
+//	@ValidNonConflictId // 아이디 중복 검사
     @Size(min = 2, max = 20, message = "아이디는 2자 이상 20자 이내로 입력해주세요.")
     @NotBlank(message = "아이디를 입력해 주세요")
 	private String mem_id;
@@ -30,7 +30,8 @@ public class MemberVO {
     @Size(min = 2, max = 10, message = "별명은 2자 이상 10자 이내로 입력해주세요")
     private String mem_nickname;
     
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$" ,message = "비밀번호는 영문, 숫자, 특수문자 포함 8~20글자 이상 입력해주세요.")
+//    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$" ,message = "비밀번호는 영문, 숫자, 특수문자 포함 8~20글자 이상 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$" ,message = "비밀번호는 8~20글자 이상 입력해주세요.")
 	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이내로 입력해주세요.")
     @NotBlank(message = "비밀번호를 입력해주세요.")
 	private String mem_passwd;
